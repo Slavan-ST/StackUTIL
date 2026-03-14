ad4512dcbff11a7bef9d48098c0c6ebbdab368c0
Slavan-ST <slavan990900000@gmail.com>
2026-03-14 18:12:25 +0700

    ScreenCaptureService - рефактор

--- StackUTIL/Services/ScreenCaptureService.cs
+++ StackUTIL/Services/ScreenCaptureService.cs
@@ -1,55 +1,10 @@
 ﻿using System.Drawing;
 using System.Drawing.Imaging;
-using System.Runtime.InteropServices;
-using System.Text;
 
 namespace DebugInterceptor.Services
 {
     public class ScreenCaptureService
     {
-        // ═══════════════════════════════════════════════════════
-        // WinAPI импорты
-        // ═══════════════════════════════════════════════════════
-
-        [DllImport("user32.dll")]
-        private static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, System.IntPtr lParam);
-
-        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
-        private static extern int GetWindowText(System.IntPtr hWnd, StringBuilder lpString, int nMaxCount);
-
-        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
-        private static extern int GetWindowTextLength(System.IntPtr hWnd);
-
-        [DllImport("user32.dll")]
-        private static extern bool GetWindowRect(System.IntPtr hWnd, out RECT lpRect);
-
-        [DllImport("user32.dll")]
-        private static extern System.IntPtr GetForegroundWindow();
-
-        [DllImport("user32.dll")]
-        private static extern bool IsWindowVisible(System.IntPtr hWnd);
-
-        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
-        private static extern int GetClassName(System.IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
-
-        [DllImport("user32.dll")]
-        private static extern bool EnumChildWindows(System.IntPtr hWndParent, EnumWindowsProc lpEnumFunc, System.IntPtr lParam);
-
-        private delegate bool EnumWindowsProc(System.IntPtr hWnd, System.IntPtr lParam);
-
-        [StructLayout(LayoutKind.Sequential)]
-        private struct RECT
-        {
-            public int Left, Top, Right, Bottom;
-            public int Width => Right - Left;
-            public int Height => Bottom - Top;
-            public Rectangle ToRectangle() => new Rectangle(Left, Top, Width, Height);
-        }
-
-        // ═══════════════════════════════════════════════════════
-        // Захват экрана и окон
-        // ═══════════════════════════════════════════════════════
-
         /// <summary>
         /// Делает скриншот всего первичного экрана
         /// </summary>
